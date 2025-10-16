89f90b0d3d00592b491515ced70cdd5001b67177
Mouncif Fahmi <mfahmi@c3r6p12.1337.ma>
2025-10-16 11:38:36 +0100

    pars with error

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 NAME = cub3d
 CFALGS = -Wall -Wextra -Werror -g3
 CC = cc
-FUN = main.c 
+FUN = main.c parsing/config_utils.c parsing/map_utils.c parsing/parse_config_and_map.c parsing/parse_cub_file.c parsing/read_lines.c
 OBJ = $(FUN:.c=.o)
 LINKING = -L/usr/include/minilibx-linux -lmlx -lXext -lX11 -lm
 all : $(NAME)
 
-$(NAME):
+$(NAME): $(OBJ)
+	
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mfahmi <mfahmi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 15:46:41 by mfahmi            #+#    #+#             */
-/*   Updated: 2025/10/02 09:50:40 by mfahmi           ###   ########.fr       */
+/*   Updated: 2025/10/15 14:15:18 by mfahmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,8 @@ int main(int ac, char **av)
 		return(printf("Erorr\nCheck File Extension\n"), 1);
 	ft_memset(&cub, 0, sizeof(t_cub));
 	if(parse_cub_file(av[1], &cub))
-	return(printf("Error Parsing\n"), 1);
-	cub.game = malloc(sizeof(t_game));
-	ft_memset(cub.game, 0, sizeof(t_game));
-	cub.game->mlx = mlx_init();
-	if(!cub.game->mlx)
-		//error
-	mlx_init_and_setup(cub.game);  //      
-    mlx_loop(cub.game->mlx);
+		return(printf("Error Parsing\n"), 1);
+	init_mlx(&cub);
+	
 }
+
