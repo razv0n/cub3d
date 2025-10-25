d8ee053cdbd2845d546fe1cfa126c239c08b306d
Mohamed Amine Wardane <mowardan@c2r1p9.1337.ma>
2025-10-25 18:46:14 +0100

    ghrayro push with segfault

--- src/game/events.c
+++ src/game/events.c
@@ -7,3 +7,21 @@ int handle_close()
     ft_free_all();
     return (0);
 }
+
+int key_press(t_cub *cub, int keycode)
+{
+    if(keycode == KEY_ESC)
+        return (handle_close());
+    cub->game->keys[keycode] = 1;
+    return (0);
+}
+
+// user Input → key_press() → keys[] updated
+//                 ↓
+//          game_loop() runs every frame
+//                 ↓
+//          Checks keys[] array
+//                 ↓
+//          Calls move_player() if needed
+//                 ↓
+//          Redraws screen
